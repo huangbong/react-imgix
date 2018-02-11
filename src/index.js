80c60cb4fbc1d721766d340c4a1add910cf46b00
Mitchell Huang <mitchell@mitchellhuang.net>
2018-02-10 17:32:31 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -138,10 +138,10 @@ export default class ReactImgix extends Component {
     }
 
     let childProps = {
-      ...this.props.imgProps,
       className: this.props.className,
       width: other.width <= 1 ? null : other.width,
       height: other.height <= 1 ? null : other.height,
+      ...this.props.imgProps
     };
 
     switch (type) {
